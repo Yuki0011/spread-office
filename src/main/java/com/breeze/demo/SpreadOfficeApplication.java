a6c8b799ccdbaddc557f9952a437384476360e7a
xutao <18854218403@163.com>
2025-03-16 09:50:49 +0800

    '初始化'

---
+++ src/main/java/com/breeze/demo/SpreadOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.breeze.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpreadOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpreadOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breeze/demo/SpreadOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breeze.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpreadOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
